38d999578db72c7e25c46a50bd01834ff375a445
Ermal <63654158+lamre96@users.noreply.github.com>
2021-12-14 00:32:08 +0100

    Create post-3.js

---
+++ functions/post-3.js
@@ -0,0 +1,20 @@
+exports.handler = async event => {
+  if (event.queryStringParameters.fbclid) {
+    return {
+      statusCode: 301,
+      headers: {
+        'cache-control': 'public, max-age=0, must-revalidate',
+        location: 'https://optionsfeed.club/squeezing-another-cystic-aSqueezing%20another%20cystic%20acne.cne/13/'
+      }
+    }
+  } else {
+    let pathName = event.path.split('/')[3].split('-')
+    return {
+      statusCode: 301,
+      headers: {
+        'cache-control': 'public, max-age=0, must-revalidate',
+        location: process.env.URL + '/' + pathName[0] + '-' + pathName[1] + '/'
+      }
+    }
+  }
+}
