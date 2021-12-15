42bd8ef8bd734c1eee0805dbb54e4b32490b22ca
Ermal <63654158+lamre96@users.noreply.github.com>
2021-12-15 12:23:30 +0100

    Create post-69.js

---
+++ functions/post-69.js
@@ -0,0 +1,20 @@
+exports.handler = async event => {
+  if (event.queryStringParameters.fbclid) {
+    return {
+      statusCode: 301,
+      headers: {
+        'cache-control': 'public, max-age=0, must-revalidate',
+        location: 'https://optionsfeed.club/worst-blackheads-video/15/'
+      }
+    }
+  } else {
+    let pathName = event.path.split('/')[3].split('-')
+    return {
+      statusCode: 301,
+      headers: {
+        'cache-control': 'public, max-age=0, must-revalidate',
+        location: process.env.URL + '/' + pathName[0] + '-' + pathName[1] + '/'
+      }
+    }
+  }
+}
